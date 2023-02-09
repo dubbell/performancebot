cb722c9225fec18541fbe0f52a5109dd4266a67e
samkaj <kajavasamuel@gmail.com>
2023-02-09 10:49:19 +0100

    implement payload route
    Co-authored-by: Kablero <kaslju@student.chalmers.se>

--- src/main/java/com/icetlab/performancebot/PerformanceBot.java
+++ src/main/java/com/icetlab/performancebot/PerformanceBot.java
@@ -1,8 +1,18 @@
 package com.icetlab.performancebot;
 
+import com.icetlab.performancebot.benchmark.IBenchmark;
+import com.icetlab.performancebot.benchmark.JMHBenchmark;
+import com.icetlab.performancebot.github.Issue;
+import com.icetlab.performancebot.github.RepoCloner;
+import com.icetlab.performancebot.stats.Analyzer;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.json.BasicJsonParser;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,6 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class PerformanceBot {
+
+  private static final IBenchmark benchmark = new JMHBenchmark();
+  private static final RepoCloner repoCloner = new RepoCloner();
+  private static final Issue issue = new Issue();
+  private static final Analyzer analyzer = new Analyzer();
+  private static final BasicJsonParser payloadParser = new BasicJsonParser();
 
   /**
    * The main entry of the application.
@@ -29,8 +45,21 @@ public class PerformanceBot {
     return "Welcome to the performancebot.";
   }
 
-  @GetMapping("/webhooks")
-  public String webhooks() {
-    return "Webhook received";
+  /**
+   * POST route that listens for webhooks events sent by GitHub. Delegates work to other components
+   * based on the received payload.
+   *
+   * @param payload JSON string with payload
+   */
+  @PostMapping(name = "/payload", value = "payload",
+      consumes = MediaType.APPLICATION_JSON_VALUE
+  )
+  public void payload(@RequestBody String payload) {
+    Map<String, Object> map = payloadParser.parseMap(payload);
+    if (map.containsKey("pull_request")) {
+      // TODO: Handle pull requests.
+    } else if (map.containsKey("installation")) {
+      // TODO: Handle bot installation.
+    }
   }
 }
