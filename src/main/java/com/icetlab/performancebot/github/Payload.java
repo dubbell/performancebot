8f804aa9b68d285e103a7b84131f5f0c5d24ba76
samkaj <kajavasamuel@gmail.com>
2023-02-21 12:19:43 +0100

    add variable with url for cloning repo

--- src/main/java/com/icetlab/performancebot/github/Payload.java
+++ src/main/java/com/icetlab/performancebot/github/Payload.java
@@ -1,6 +1,5 @@
 package com.icetlab.performancebot.github;
 
-
 import static com.icetlab.performancebot.PerformanceBot.getIssue;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -52,6 +51,12 @@ public class Payload {
     JsonNode node = getPayloadAsNode(payload);
     String installationId = node.get("installation").get("id").asText();
     String issuesUrl = node.get("pull_request").get("issue_url").asText();
+
+    // This url is used to clone the repo.
+    // TODO: Handle the repo to clone in a container, feel free to do whatever you
+    // want, this just how to obtain it.
+    String repoUrl = node.get("pull_request").get("head").get("repo").get("clone_url").asText();
+
     issuesUrl = issuesUrl.substring(0, issuesUrl.lastIndexOf("/"));
     getIssue().createIssue(issuesUrl, "hello", "my name is performancebot", installationId);
   }
