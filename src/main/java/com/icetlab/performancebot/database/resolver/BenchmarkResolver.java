ea27621b8a961c4bbdc2b2d317758434e145dd61
samkaj <kajavasamuel@gmail.com>
2023-02-22 17:50:39 +0100

    Add graphql resolvers

---
+++ src/main/java/com/icetlab/performancebot/database/resolver/BenchmarkResolver.java
@@ -0,0 +1,61 @@
+package com.icetlab.performancebot.database.resolver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import com.icetlab.performancebot.database.model.Benchmark;
+import com.icetlab.performancebot.database.repository.BenchmarkRepository;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+@Controller
+public class BenchmarkResolver {
+
+  @Autowired
+  private BenchmarkRepository benchmarkRepository;
+
+  public Benchmark getBenchmarkById(String id) {
+    return benchmarkRepository.findById(id).orElse(null);
+  }
+
+  public void addBenchmark(String id, String runData, String projectId) {
+    benchmarkRepository.insert(new Benchmark(id, runData, projectId));
+  }
+
+  @Bean
+  public GraphQLSchema benchmarkSchema() {
+    TypeDefinitionRegistry typeRegistry = new TypeDefinitionRegistry();
+
+    String schema = "type Benchmark {\n" + "  id: ID!\n" + "  runData: String!\n"
+        + "  projectId: String!\n" + "}\n" + "type Query {\n" + "  benchmark(id: ID!): Benchmark\n"
+        + "}\n" + "type Mutation {\n"
+        + "  addBenchmark(id: ID!, runData: String!, projectId: String!): Benchmark\n" + "}\n";
+    typeRegistry.merge(new SchemaParser().parse(schema));
+
+    RuntimeWiring wiring = RuntimeWiring.newRuntimeWiring()
+        .type("Benchmark",
+            builder -> builder
+                .dataFetcher("id", environment -> ((Benchmark) environment.getSource()).getId())
+                .dataFetcher("runData",
+                    environment -> ((Benchmark) environment.getSource()).getRunData())
+                .dataFetcher("projectId",
+                    environment -> ((Benchmark) environment.getSource()).getProjectId()))
+        .type("Query",
+            builder -> builder.dataFetcher("benchmark",
+                environment -> getBenchmarkById(environment.getArgument("id"))))
+        .type("Mutation", builder -> builder.dataFetcher("addBenchmark", environment -> {
+          String id = environment.getArgument("id");
+          String runData = environment.getArgument("runData");
+          String projectId = environment.getArgument("projectId");
+          addBenchmark(id, runData, projectId);
+          return getBenchmarkById(id);
+        })).build();
+
+    SchemaGenerator schemaGenerator = new SchemaGenerator();
+    return schemaGenerator.makeExecutableSchema(typeRegistry, wiring);
+  }
+}
---
+++ src/main/java/com/icetlab/performancebot/database/resolver/GitHubProjectResolver.java
@@ -0,0 +1,82 @@
+package com.icetlab.performancebot.database.resolver;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import com.icetlab.performancebot.database.model.Benchmark;
+import com.icetlab.performancebot.database.model.GitHubProject;
+import com.icetlab.performancebot.database.repository.GitHubProjectRepository;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+@Controller
+public class GitHubProjectResolver {
+  String schema = """
+      type Query {
+        projects: [GitHubProject]
+      }
+
+      type GitHubProject {
+        id: ID!
+        name: String!
+        owner: String!
+        url: String!
+        runs: [Benchmark]
+      }
+
+      type Benchmark {
+        id: ID!
+        name: String!
+      }
+      """;
+
+  @Autowired
+  private GitHubProjectRepository gitHubProjectRepository;
+
+  public List<GitHubProject> getProjects() {
+    return gitHubProjectRepository.findAll();
+  }
+
+  public GitHubProject getProjectById(String id) {
+    Optional<GitHubProject> project = gitHubProjectRepository.findById(id);
+    if (project.isPresent()) {
+      return project.get();
+    }
+    return null;
+  }
+
+  public List<Benchmark> getRunsById(String id) {
+    return gitHubProjectRepository.findAllRunsById(id);
+  }
+
+  public void addProject(String id, String name, String owner, String url) {
+    Optional<GitHubProject> project = gitHubProjectRepository.findById(id);
+    if (project.isPresent()) {
+      return;
+    }
+    gitHubProjectRepository.insert(new GitHubProject(id, name, owner, url, new ArrayList<>()));
+  }
+
+  @Bean
+  public GraphQLSchema githubProjectSchema() {
+    TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(schema);
+    RuntimeWiring wiring = RuntimeWiring.newRuntimeWiring()
+        .type("Query", builder -> builder.dataFetcher("projects", environment -> getProjects()))
+        .type("GitHubProject", builder -> builder
+            .dataFetcher("id", environment -> ((GitHubProject) environment.getSource()).getId())
+            .dataFetcher("name", environment -> ((GitHubProject) environment.getSource()).getName())
+            .dataFetcher("owner",
+                environment -> ((GitHubProject) environment.getSource()).getOwner())
+            .dataFetcher("url", environment -> ((GitHubProject) environment.getSource()).getUrl())
+            .dataFetcher("runs",
+                environment -> ((GitHubProject) environment.getSource()).getRuns()))
+        .build();
+    return new SchemaGenerator().makeExecutableSchema(typeRegistry, wiring);
+  }
+}
