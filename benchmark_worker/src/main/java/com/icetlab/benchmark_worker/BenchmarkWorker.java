4b2751a7828625912f6ac31b64f3dd41e38f235f
Linus Lundgren <linus.c.lundgren@gmail.com>
2023-02-23 08:49:07 +0100

    compilation of cloned project

--- benchmark_worker/src/main/java/com/icetlab/benchmark_worker/BenchmarkWorker.java
+++ benchmark_worker/src/main/java/com/icetlab/benchmark_worker/BenchmarkWorker.java
@@ -1,7 +1,11 @@
 package com.icetlab.benchmark_worker;
 
 import java.io.IOException;
+import java.util.Collections;
 import org.apache.commons.io.FileUtils;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.Invoker;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
@@ -13,6 +17,9 @@ import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
 
 
 /**
@@ -35,58 +42,84 @@ public class BenchmarkWorker {
   /**
    * Listens for new tasks from the performancebot.
    */
-  @PostMapping(name = "/benchmark", value = "benchmark", consumes = MediaType.APPLICATION_JSON_VALUE)
-  public void benchmark(@RequestBody String task) {
+  @PostMapping(name = "/task", value = "task", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public void startTask(@RequestBody String task) {
     JacksonJsonParser parser = new JacksonJsonParser();
 
-    String repoName = (String) parser.parseMap(task).get("name");
     String repoURL = (String) parser.parseMap(task).get("url");
     String accessToken = (String) parser.parseMap(task).get("token");
 
-    cloneRepo(repoName, repoURL, accessToken);
+    // if one thing fails, the benchmark is cancelled
+    try {
+      clone(repoURL, accessToken);
+      compile();
 
-    // TODO run benchmark
-    // TODO send to database
-    // TODO analysis
-    // TODO post issue
+      // TODO run benchmark
+      // TODO send to database
+      // TODO analysis
+      // TODO post issue
+    }
+    catch (Exception e) {}
 
-    deleteRepo(repoName);
+    delete();
   }
 
   /**
    * Creates directory and clones repository into it.
-   * @param repoName name of the repository
    * @param repoURL repository url
    * @param accessToken repository access token for authentication
    */
-  private void cloneRepo(String repoName, String repoURL, String accessToken) {
+  private void clone(String repoURL, String accessToken) throws Exception {
 
     // creates directory
-    File dir = new File(repoName);
-    dir.mkdir();
+    File dir = new File("benchmark_directory");
+    if (!dir.mkdir()) // attempts to create directory
+      return; // TODO error handling?
 
+    CredentialsProvider credentials = new UsernamePasswordCredentialsProvider(accessToken, "");
+    Git.cloneRepository()
+      .setCredentialsProvider(credentials) // if the repository is private, the access token should authorize the request
+      .setURI(repoURL)
+      .setDirectory(dir)
+      .call();
+  }
 
-    try {
-      CredentialsProvider credentials = new UsernamePasswordCredentialsProvider(accessToken, "");
-      Git.cloneRepository()
-          .setCredentialsProvider(credentials)
-          .setURI(repoURL)
-          .setDirectory(dir)
-          .call();
-    } catch (Exception e) {}
+  /**
+   * Compiles the cloned repository by executing a new Maven build to the 'verify' phase.
+   */
+  private void compile() throws Exception {
+    // construct request to clean target directory
+    InvocationRequest cleanRequest = new DefaultInvocationRequest();
+    cleanRequest.setPomFile(new File("benchmark_directory/pom.xml"));
+    cleanRequest.setGoals(Collections.singletonList("clean"));
 
-    // TODO error handling
+    // construct request to compile project
+    InvocationRequest verifyRequest = new DefaultInvocationRequest();
+    verifyRequest.setPomFile(new File("benchmark_directory/pom.xml"));
+    verifyRequest.setGoals(Collections.singletonList("verify"));
+
+    // cleans and then compiles project
+    Invoker invoker = new DefaultInvoker();
+    InvocationResult cleanResult = invoker.execute(cleanRequest);
+    InvocationResult verifyResult = invoker.execute(verifyRequest);
+
+    // checks if either of the requests failed
+    if (cleanResult.getExitCode() != 0 || verifyResult.getExitCode() != 0)
+      throw new Exception("Build failed.");
+  }
+
+  private void benchmark() throws Exception {
+
   }
 
   /**
    * Deletes local clone of repository with the given name.
-   * @param repoName name of the repository
    */
-  private void deleteRepo(String repoName) {
+  private void delete() {
     try {
-      FileUtils.deleteDirectory(new File(repoName));
-    } catch (IOException e) {}
+      FileUtils.deleteDirectory(new File("benchmark_directory"));
+    } catch (IOException ignored) {}
 
-    // TODO error handling
+    // TODO error handling?
   }
 }
