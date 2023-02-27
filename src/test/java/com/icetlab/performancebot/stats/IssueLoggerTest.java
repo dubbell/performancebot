15d3b7aa17ba52c9c8c2e354432612eaa87aee57
elinhagman <73999747+elinhagman@users.noreply.github.com>
2023-02-27 14:15:10 +0100

    Add test classes for stats package

---
+++ src/test/java/com/icetlab/performancebot/stats/IssueLoggerTest.java
@@ -0,0 +1,13 @@
+package com.icetlab.performancebot.stats;
+
+public class IssueLoggerTest {
+
+  // TODO: createSimpleIssue(String json) - Check that valid json string's score is equal to the
+  //  score in the string that is returned
+  // TODO: createSimpleIssue(String json) - Check that valid json string's benchmark is equal to
+  //  benchmark in the string that is returned
+  // TODO: createSimpleIssue(String json) - Check that non valid json string input throws exception
+  
+  // TODO: storeIssue(String issue) - test that a valid input does not throw exception
+
+}
---
+++ src/test/java/com/icetlab/performancebot/stats/SimpleBenchmarkIssueTest.java
@@ -0,0 +1,9 @@
+package com.icetlab.performancebot.stats;
+
+public class SimpleBenchmarkIssueTest {
+
+  // TODO: formatBenchmarkIssue - Check that score in JMHBenchmark is equal to score in String that is returned
+  // TODO: formatBenchmarkIssue - Check that benchmark in JMHBenchmark is equal to benchmark in String that is returned
+  // TODO: Test empty array input
+
+}
