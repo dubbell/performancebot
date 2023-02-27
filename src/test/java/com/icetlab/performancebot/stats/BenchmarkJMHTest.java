5f1481921b3dc5190f982881920b7ed385b0b265
elinhagman <73999747+elinhagman@users.noreply.github.com>
2023-02-27 14:15:10 +0100

    Add test classes for stats package

---
+++ src/test/java/com/icetlab/performancebot/stats/BenchmarkJMHTest.java
@@ -0,0 +1,9 @@
+package com.icetlab.performancebot.stats;
+
+public class BenchmarkJMHTest {
+
+  // TODO: Test that a valid json string can be parsed to a BenchmarkJMH object with ObjectMapper
+  // TODO: Test that a non valid json string cannot be parsed to a BenchmarkJMH with ObjectMapper
+
+
+}
