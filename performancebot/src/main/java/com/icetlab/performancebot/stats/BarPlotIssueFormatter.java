a535b14bdc556fb63bb424ddc04dc5056e2bedb2
samkaj <kajavasamuel@gmail.com>
2023-04-18 16:55:36 +0200

    Add functionality for images in issues
    Co-authored-by: elinhagman <73999747+elinhagman@users.noreply.github.com>

--- performancebot/src/main/java/com/icetlab/performancebot/stats/BarPlotIssueFormatter.java
+++ performancebot/src/main/java/com/icetlab/performancebot/stats/BarPlotIssueFormatter.java
@@ -2,11 +2,9 @@ package com.icetlab.performancebot.stats;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 import org.knowm.xchart.BitmapEncoder;
 import org.knowm.xchart.CategoryChart;
 import org.knowm.xchart.CategoryChartBuilder;
@@ -35,30 +33,81 @@ public class BarPlotIssueFormatter implements BenchmarkIssueFormatter {
       node = new ObjectMapper().readTree(jmhResults);
       String installationId = node.get("installation_id").asText();
       String repoId = node.get("repo_id").asText();
-      List<String> methodNames = FormatterUtils.getMethodsFromCurrentRun(jmhResults);
-      Set<Method> methods = installationService.getMethodsFromRepo(installationId, repoId).stream()
-          .filter(method -> methodNames.contains(method.getMethodName()))
-          .collect(Collectors.toSet());
-      Map<String, List<Method>> methodMap = FormatterUtils.groupMethodsByClassName(methods);
-      for (String className : methodMap.keySet()) {
-        createCategoryChartFromMap(className, methodMap.get(className));
-      }
-      return "TODO: MD formatted string.";
+      List<String> methodNames = FormatterUtils.getMethodNamesFromCurrentRun(jmhResults);
+      Set<Method> methods = FormatterUtils.filterMethodsFromCurrentRun(
+          installationService.getMethodsFromRepo(installationId, repoId), methodNames);
+      Map<String, List<Method>> classes = FormatterUtils.groupMethodsByClassName(methods);
+      String issueBody = formatIssueBody(classes);
+      return issueBody;
     } catch (JsonProcessingException e) {
       return e.toString();
     }
   }
 
-  private void createCategoryChartFromMap(String className, List<Method> methods) {
-    List<Result> results = new ArrayList<>();
-    for (Method method : methods) {
-      method.getRunResults().forEach((x) -> results.add(x));
-      createImageFromResults(results, method.getMethodName());
-      results.clear();
+  /**
+   * <p>
+   * Builds an issue based on the structure of the classes map
+   * </p>
+   * 
+   * <pre>
+   * <code>
+   * for class in classes:
+   *   # ClassName
+   *   for method in methods:
+   *     ## MethodName
+   *     ![methodName](url)
+   *     other run information
+   * </code>
+   * </pre>
+   *
+   * @param classes the classes and their methods to be formatted
+   * @return the classes
+   */
+  private String formatIssueBody(Map<String, List<Method>> classes) {
+    StringBuilder sb = new StringBuilder();
+    for (String className : classes.keySet()) {
+      sb.append(String.format("# %s\n", className));
+      for (Method method : classes.get(className)) {
+        sb.append(formatMethodBody(method));
+      }
     }
+    return sb.toString();
   }
 
-  private void createImageFromResults(List<Result> results, String header) {
+  /**
+   * Takes a method and returns a markdown String containing method header, run results plot and
+   * configuration results
+   * 
+   * @return
+   */
+  private String formatMethodBody(Method method) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(String.format("## %s\n", method.getMethodName()));
+    String path;
+    try {
+      path = createImageFromResults(method.getRunResults(), method.getMethodName());
+    } catch (IOException e) {
+      path =
+          "https://ih1.redbubble.net/image.1539738010.3563/flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg";
+    }
+    sb.append(String.format("![%s](%s)\n", method.getMethodName(), uploadImageAndGetUrl(path)));
+    sb.append("TODO: implement additional info\n");
+    return sb.toString();
+  }
+
+  private String uploadImageAndGetUrl(String path) {
+    // TODO: upload to imagekit and get url
+    return path;
+  }
+
+  /*
+   * private void createCategoryChartFromMap(String className, List<Method> methods) { List<Result>
+   * results = new ArrayList<>(); for (Method method : methods) {
+   * results.addAll(method.getRunResults()); createImageFromResults(results,
+   * method.getMethodName()); results.clear(); } }
+   */
+
+  private String createImageFromResults(List<Result> results, String header) throws IOException {
     CategoryChart chart = new CategoryChartBuilder().width(600).height(600).title(header)
         .xAxisTitle("Date").yAxisTitle("Score").build();
     List<String> timestamps = results.stream()
@@ -67,14 +116,19 @@ public class BarPlotIssueFormatter implements BenchmarkIssueFormatter {
         .map((x) -> Double.parseDouble(FormatterUtils.getScoreFromPrimaryMetric(x.getData())))
         .toList();
     chart.addSeries(header, timestamps, score);
-    writeChartToPng(chart, header);
+    return writeChartToPng(chart, header);
   }
 
-  private void writeChartToPng(CategoryChart chart, String path) {
-    try {
-      BitmapEncoder.saveBitmapWithDPI(chart, "./" + path, BitmapFormat.PNG, 300);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+  /**
+   * Converts the chart to a PNG image and returns the path to it.
+   * 
+   * @param chart the chart to be converted to an PNG image
+   * @param fileName the name of the PNG file
+   * @return the path of the PNG image of the bar chart
+   */
+  private String writeChartToPng(CategoryChart chart, String fileName) throws IOException {
+    BitmapEncoder.saveBitmapWithDPI(chart, fileName, BitmapFormat.PNG, 300);
+    return fileName + ".png";
+
   }
 }
