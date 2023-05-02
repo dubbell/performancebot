af9dd5cb6d0f0a2b7c4f5f5c910e2171f62dced7
LinderJonathan <linderjonathan3@gmail.com>
2023-05-02 22:48:58 +0200

    Moved test to new methodByClassFormatterTest.java
    TODO: fix issue with spring

--- performancebot/src/test/java/com/icetlab/performancebot/stats/MethodByClassFormatterTest.java
+++ performancebot/src/test/java/com/icetlab/performancebot/stats/MethodByClassFormatterTest.java
@@ -1,6 +1,8 @@
 package com.icetlab.performancebot.stats;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.spy;
 
 import java.io.IOException;
@@ -10,11 +12,13 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import com.icetlab.performancebot.database.controller.InstallationController;
 import com.icetlab.performancebot.database.model.GitHubRepo;
@@ -23,6 +27,7 @@ import com.icetlab.performancebot.database.model.Method;
 import com.icetlab.performancebot.database.model.Result;
 import com.icetlab.performancebot.database.service.InstallationService;
 import com.icetlab.performancebot.webhook.PayloadManager;
+import org.springframework.stereotype.Component;
 
 @SpringBootTest
 public class MethodByClassFormatterTest {
@@ -120,5 +125,48 @@ public class MethodByClassFormatterTest {
     Assert.assertTrue(md.contains("# AnotherClassName"));
   }
 
+  /*
+    Test that two classes containing a method with the same name formats correctly
+   */
+  /*@Test
+  public void testNoDuplicateMethods() {
+    List<Result> oldWayResults, newWayResults, otherResults;
+    oldWayResults = new ArrayList<>();
+    newWayResults = new ArrayList<>();
+    otherResults = new ArrayList<>();
+    oldWayResults.add(new Result(Constants.exampleResultSampleBenchmarkOldWay));
+    oldWayResults.add(new Result(Constants.bmResultSampleBenchmarkOldWay));
+    newWayResults.add(new Result(Constants.bmResultSampleBenchmarkNewWay)); //duplicate
+    otherResults.add(new Result(Constants.bmResultAnotherClassNameNewWay));
 
+    installationService.addMethodToRepo("an id", "a repo id",
+            new Method("com.szatmary.peter.SampleBenchmarkTest.oldWay", oldWayResults));
+    installationService.addMethodToRepo("an id", "a repo id",
+            new Method("com.szatmary.peter.SampleBenchmarkTest.newWay", newWayResults));
+    installationService.addMethodToRepo("an id", "a repo id",
+            new Method("com.szatmary.peter.AnotherClassName.newWay", otherResults));
+
+    String firstClassNameHeader = "# SampleBenchmarkTest";
+    String lastClassNameHeader = "# AnotherClassName";
+    String methodDuplicate = "## newWay";
+
+    String md = methodByClassFormatter.formatBenchmarkIssue(Constants.bmResultMultipleClasses).strip();
+
+    int firstMethodPos = md.indexOf(methodDuplicate);
+    int lastMethodPos = md.indexOf(methodDuplicate, firstMethodPos + 1);
+    int firstClassPos = md.indexOf(firstClassNameHeader);
+    int lastClassPos = md.indexOf(lastClassNameHeader);
+
+    // check that duplicate method is in markdown exactly two times
+    Assertions.assertTrue(firstMethodPos < lastMethodPos);
+    assertNotEquals(lastMethodPos, -1);
+    assertEquals(md.indexOf(methodDuplicate, lastMethodPos + 1), -1);
+
+    // check that the first one is sub header of first class
+    Assertions.assertTrue((firstMethodPos > firstClassPos) && (firstMethodPos < lastClassPos));
+
+    // check that the last one is sub header of the second class
+    Assertions.assertTrue((lastMethodPos > firstClassPos) && (lastMethodPos > lastClassPos));
+
+  }*/
 }
